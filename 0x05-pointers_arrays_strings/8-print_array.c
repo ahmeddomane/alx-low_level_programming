7d956ea4f0cf8fb0e45f4d413667e03e78db0815
ahmeddomane <midndb2000@gmail.com>
2023-08-22 00:34:47 +0000

    d

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,6 +4,7 @@
  * print_array - prints n elements of an array of integers.
  * @n: elements parameter input
  * @a: string parameter input
+ * Return: Nothing
 */
 
 void print_array(int *a, int n)
