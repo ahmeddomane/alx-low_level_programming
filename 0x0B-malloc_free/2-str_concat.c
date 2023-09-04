6a8c43309cba09de50ca5ca31fab9eccd601018a
ahmeddomane <midndb2000@gmail.com>
2023-09-04 15:53:32 +0000

    d

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -38,10 +38,10 @@ char *str_concat(char *s1, char *s2)
 	if (m == 0)
 		return (0);
 
-	for (i = 0; i <= sizel + sizel; i++)
+	for (i = 0; i <= sizel + size2; i++)
 	{
-		if (i < size2)
-			m[i] = s2[i];
+		if (i < sizel)
+			m[i] = sl[i];
 		else
 			m[i] = s2[i - sizel];
 	}
