f5d62001a2a9431ae1483f1d326657a9905d754f
ahmeddomane <midndb2000@gmail.com>
2023-09-08 02:34:03 +0000

    d

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,43 +49,43 @@ int is_digit(char c)
 
 void multiply(char *num1, char *num2)
 {
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	int len_result = len1 + len2;
-	int *result = malloc(sizeof(int) * len_result);
+int len1 = strlen(num1);
+int len2 = strlen(num2);
+int len_result = len1 + len2;
+int *result = malloc(sizeof(int) * len_result);
 
-	if (result == NULL)
-		printf("Error\n");
-		exit(98);
-	/*Initialize the result array to 0*/
+if (result == NULL)
+	printf("Error\n");
+	exit(98);
+/*Initialize the result array to 0*/
 
-	for (int i = 0; i < len_result; i++)
-		result[i] = 0;
-	 /*Multiply the digits and add to the result*/
-	for (int i = len1 - 1; i >= 0; i--)
-	{
-		for (int j = len2 - 1; j >= 0; j--)
-			int product = (num1[i] - '0') * (num2[j] - '0');
-			int sum = product + result[i + j + 1];
+for (int i = 0; i < len_result; i++)
+	result[i] = 0;
+ /*Multiply the digits and add to the result*/
+for (int i = len1 - 1; i >= 0; i--)
+{
+	for (int j = len2 - 1; j >= 0; j--)
+		int product = (num1[i] - '0') * (num2[j] - '0');
+		int sum = product + result[i + j + 1];
 
-			result[i + j + 1] = sum % 10;
-			result[i + j] += sum / 10;
-	}
-	/*Find the first non-zero digit in the result*/
-	int start = 0;
+		result[i + j + 1] = sum % 10;
+		result[i + j] += sum / 10;
+}
+/*Find the first non-zero digit in the result*/
+int start = 0;
 
-	while (start < len_result && result[start] == 0)
-		start++;
-	/*Print the result*/
-	if (start == len_result)
-		printf("0\n");
-	else
-	{
-		for (int i = start; i < len_result; i++)
-			printf("%d", result[i]);
-	printf("\n");
-	}
-	free(result);
+while (start < len_result && result[start] == 0)
+	start++;
+/*Print the result*/
+if (start == len_result)
+	printf("0\n");
+else
+{
+	for (int i = start; i < len_result; i++)
+		printf("%d", result[i]);
+printf("\n");
+}
+free(result);
 }
 /**
  * main - The main function of the program.
