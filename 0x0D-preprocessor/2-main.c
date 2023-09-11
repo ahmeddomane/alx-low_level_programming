9140a6b40ccfc50aba69d7c2f5947de8d19cc20c
ahmeddomane <midndb2000@gmail.com>
2023-09-11 14:32:24 +0000

    d

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stido.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always returns 0 to indicate success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
