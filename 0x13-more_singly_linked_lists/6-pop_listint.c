2cd259eb96f04217590570f2f249efec9a562581
ahmeddomane <midndb2000@gmail.com>
2023-09-25 23:07:45 +0000

    fd

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,4 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
  * pop_listint - pops head node of list
--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,4 +1,4 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
  * sum_listint - returns sum of all data
